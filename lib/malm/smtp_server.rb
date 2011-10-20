5b1b1260620246ec82cfc0157d2378f76c2d2b6a
madlep <julian.doherty.ml@gmail.com>
2011-10-20 13:30:43 +1100

    renamed SMTPServer "Session" to "Message"

--- lib/malm/smtp_server.rb
+++ lib/malm/smtp_server.rb
@@ -7,7 +7,7 @@ class Malm
     attr_accessor :mail_log
     attr_accessor :message_db
   
-    class Session
+    class Message
       attr_accessor :data_mode, :data_mode, :email_body, :mail_from, :rcpt_to, :subject
       alias :data_mode? :data_mode
     
@@ -19,65 +19,65 @@ class Malm
     end
   
     def serve(io)
-      session = Session.new
-      puts "Connected"
+      message = Message.new
+      log "Connected"
       io.print "220 hello\r\n"
       loop do
         data = io.gets
-        puts ">>" + data
-        ok, op = process_line(data, session)
-        puts "<<" + op 
+        log ">>" + data
+        ok, op = process_line(data, message)
+        log "<<" + op 
         io.print op               
         break unless ok
         break if io.closed?
       end
       begin
         io.close
-        db_insert(session)
+        db_insert(message)
       rescue => e
         log "something screwed up..."
         log e.backtrace
       end
     end
 
-    def process_line(line, session)
-      if (session.data_mode?) && (line.chomp =~ /^\.$/)
-        session.data_mode = false
+    def process_line(line, message)
+      if (message.data_mode?) && (line.chomp =~ /^\.$/)
+        message.data_mode = false
         return true, "250 OK\r\n"
-      elsif session.data_mode?
-        session.email_body += line
+      elsif message.data_mode?
+        message.email_body += line
         return true, ""
       elsif (line =~ /^(HELO|EHLO)/)
         return true, "250 and..?\r\n"
       elsif (line =~ /^QUIT/)
         return false, "221 bye\r\n"
       elsif (line =~ /^MAIL FROM\:/)
-        session.mail_from = (/^MAIL FROM\:<(.+)>.*$/).match(line)[1]
+        message.mail_from = (/^MAIL FROM\:<(.+)>.*$/).match(line)[1]
         return true, "250 OK\r\n"
       elsif (line =~ /^RCPT TO\:/)
-        session.rcpt_to = (/^RCPT TO\:<(.+)>.*$/).match(line)[1]
+        message.rcpt_to = (/^RCPT TO\:<(.+)>.*$/).match(line)[1]
         return true, "250 OK\r\n"
       elsif (line =~ /^DATA/)
-        session.data_mode = true
+        message.data_mode = true
         return true, "354 Enter message, ending with \".\" on a line by itself\r\n"
       else
         return true, "500 ERROR\r\n"
       end
     end
   
-    def db_insert(session)
+    def db_insert(message)
       subject_regex = /^Subject\: (.+)$/
 
-      subject_match = subject_regex.match(session.email_body)
+      subject_match = subject_regex.match(message.email_body)
       subject = subject_match ? subject_match[1] : ""
       subject.strip!
     
-      message = {:subject => subject, :from => session.mail_from, :to => session.rcpt_to, :body => session.email_body}
+      message_data = {:subject => subject, :from => message.mail_from, :to => message.rcpt_to, :body => message.email_body}
     
-      @mail_log_fd.puts(message.inspect) if @mail_log_fd
-      @message_db.create(message) if @message_db
+      @mail_log_fd.puts(message_data.inspect) if @mail_log_fd
+      @message_db.create(message_data) if @message_db
     
-      log("Message received: #{message.inspect}")
+      log("Message received: #{message_data.inspect}")
     end
   
     protected
